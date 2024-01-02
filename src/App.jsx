0c693dff2ff048fc4d296c3e461f207bc6f30a33
Sulilium <lilyjennyw@gmail.com>
2024-01-02 01:49:43 -0500

    Transactions portion started

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,55 @@ export default function App(){
         <h2 className="amounts">- {moneySpent}</h2>
       </div>
     </div>
+
+    {/* Shows transactions from the current month */}
+    <div id="transactions">
+      <button type="button">Add Transaction</button>
+      <table>
+        <tr>
+          <th>Service</th>
+          <th>Description</th>
+          <th>Category</th>
+          <th>Date</th>
+          <th>Amount</th>
+        </tr>
+        <tr>
+          <td><input type="text" placeholder="Service"></input></td>
+          <td><input type="text" placeholder="Description"></input></td>
+          <td><select>
+                <option>Entertainment</option>
+                <option>Food</option>
+                <option>Miscellaneous</option>
+                <option>option...</option>
+              </select></td>
+          <td>[Current Date]</td>
+          <td>$ <input type="text" placeholder="Amount"></input></td>
+        </tr>
+        <tr>
+          <td>Steam</td>
+          <td>Hades lol</td>
+          <td>Entertainment</td>
+          <td>Jan 22th</td>
+          <td>$ 36.73</td>
+        </tr>
+        <tr>
+          <td>stuff</td>
+          <td>desc</td>
+          <td>Miscellaneous</td>
+          <td>Jan 3th</td>
+          <td>$ 20.00</td>
+        </tr>
+        <tr>
+          <td>stuff again</td>
+          <td>desc</td>
+          <td>Miscellaneous</td>
+          <td>Jan 1st</td>
+          <td>$ 22.70</td>
+        </tr>
+      </table>
+    </div>
     </>
   )
-
 }
 
 

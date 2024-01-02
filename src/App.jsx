b99d194c2353baed348911b0485e58049fb13895
Sulilium <lilyjennyw@gmail.com>
2024-01-01 23:50:06 -0500

    basic side content skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import "./styles.css"
+
 export default function App(){
   const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
   let monthName = month[(new Date()).getMonth()];
+  let moneyGained = 0;
+  let moneySpent = 0;
 
   return (
     <>
-    <h1 id="month">{monthName}</h1>
-    <div></div>
+    <div id="side-content">
+      <h1 id="month" className="heading">Month of {monthName}</h1>
+      
+      <div>
+        <h1>Money Gained</h1>
+        <h2>+{moneyGained}</h2>
+      </div>
+
+      <div>
+        <h1>Money Spent</h1>
+        <h2>-{moneySpent}</h2>
+      </div>
+    </div>
     </>
   )
 

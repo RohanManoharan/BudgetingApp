7edf383e09ea5b938412dc485244d19df5710f73
Sulilium <lilyjennyw@gmail.com>
2024-01-13 18:06:16 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/RohanManoharan/BudgetingApp""
    This reverts commit 534f795af65dddfbdaa6ca029514d9f5e4ab9548.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,24 +1,28 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
+import Money_SideBar from "./components/Home/Money_SideBar";
+import Transaction_UI from "./components/Home/Transaction_UI";
 import "./styles.css"
-import { Money_SideBar } from "./components/Home/Money_SideBar";
-import { Transaction_Table } from "./components/Home/Transaction_Table";
 
 // Homepage of the App
-export default function Home(){
+export default function Home({ nameReg }){
   // Variables to get information related to dates
   const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
   let monthName = month[(new Date()).getMonth()];
   let date = (new Date()).toDateString().slice(4, 10).replace(/\b0/, '');
 
+  // State setup
   const[money, setMoney] = useState({ total: 0, gained: 0, spent: 0 });
   const[showTForm, setShowTForm] = useState(false);
   const[transactions, setTransactions] = useState([]);
   
+  // Adds the new transaction described by the arguments to the transactions list and updates the money accordingly
   function addTransaction(service, description, category, day, price){
+    // Returns a new array with all previous transaction in addition to the new transaction
     setTransactions(currentTransactions => {
       return [...currentTransactions, { id: crypto.randomUUID(), service: service, desc: description, category: category, day: day, price: price }]
     })
+    // Decides how to update money depending if the transaction price is negative or positive
     parseFloat(price) < 0 ? (setMoney(currentMoney => {
       return {total: currentMoney.total+parseFloat(price), gained: currentMoney.gained, spent: currentMoney.spent+parseFloat(price)}
     })) : (
@@ -28,13 +32,9 @@ export default function Home(){
     )
   }
 
-  function deleteTransaction(id){
-    let price = 0
-    transactions.forEach(transaction => {
-      if(transaction.id === id){
-        price = transaction.price
-      }
-    })
+  // Deletes the transaction specified by the id from the list and updates the money accodingly
+  function deleteTransaction(id, price){
+    // Decides how to update money depending if the transaction price is negative or positive
     parseFloat(price) < 0 ? (setMoney(currentMoney => {
       return {total: currentMoney.total-parseFloat(price), gained: currentMoney.gained, spent: currentMoney.spent-parseFloat(price)}
     })) : (
@@ -42,7 +42,8 @@ export default function Home(){
         return {total: currentMoney.total-parseFloat(price), gained: currentMoney.gained-parseFloat(price), spent: currentMoney.spent}
       }))
     )
-
+    
+    // Filters the array so that it excludes the transaction with the specified id
     setTransactions(currentTransactions => {
       return currentTransactions.filter(transaction => transaction.id !== id)
     })
--- src/components/Home/Transaction_Item.jsx
+++ src/components/Home/Transaction_Item.jsx
@@ -1,12 +1,13 @@
-export function Transaction_Item({ transaction, month, deleteTransaction }){
+// Component that renders a transaction as a table row
+export default function Transaction_Item({ transaction, month, deleteTransaction }){
     return(
-        <tr key={transaction.id}>
-            <td id="transaction-service">{transaction.service}</td>
-            <td id="transaction-desc">{transaction.desc}</td>
-            <td id="transaction-category" >{transaction.category}</td>
-            <td id="transaction-day">{month} {transaction.day}</td>
-            <td id="transaction-price">$ {transaction.price}</td>
-            <td><button className="transaction-button" onClick={() => deleteTransaction(transaction.id)}>Delete</button></td>
+        <tr>
+            <td id="transaction-service" className="table-data" >{transaction.service}</td>
+            <td id="transaction-desc" className="table-data" >{transaction.desc}</td>
+            <td id="transaction-category" className="table-data" >{transaction.category}</td>
+            <td id="transaction-day" className="table-data" >{month} {transaction.day}</td>
+            <td id="transaction-price" className="table-data" >$ {transaction.price}</td>
+            <td><button id="delete-button" className="button transaction-button" onClick={() => deleteTransaction(transaction.id, transaction.price)}>Delete</button></td>
         </tr>
     )
 }

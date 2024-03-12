eb78d631c2e51e2135e380024cf0f4a7363f3aba
RohanManoharan <rohan.manoharan@ryerson.ca>
2024-03-11 20:26:06 -0400

    re init

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export default function Navbar(){
             <ul className='navbar-menu'>
                 <li><a href="/home">Home</a></li>
                 {/* <li><a href="/Monthly-Planner">Monthly Planner</a></li> */}
-                <li><a href="/contact">Contact</a></li>
+                <li><a href="/about">About</a></li>
             </ul>
     </div>
     )
